6d7272ef86ed6a97c8fdfd625a0d8fed9830a2bc
justin-hubbard <jdawggey@gmail.com>
2025-03-24 23:32:13 -0700

    Start of bracket logic and color conversion
    - Util functions for converting hex to rgb and hsv
    - bracket working up to elite 8, probably fine up to chip
    - placeholder for color comparison function, is passed in so testing any comp function is possible

---
+++ colors.py
@@ -0,0 +1,47 @@
+def hexToRGB(hex):
+    return tuple(int(hex[i:i+2], 16) for i in (1,3,5))
+
+def RGBtoHSV(rgb):
+
+    #normalize
+    red = rgb[0] / 255
+    green = rgb[1] / 255
+    blue = rgb[2] / 255
+
+    cmax = max(red, green, blue)
+    cmin = min(red, green, blue)
+
+    delta = cmax - cmin
+
+    #calculate hue
+    hue = None
+    if cmax == red:
+        hue = 60 * (((green - blue) / delta ) % 6)
+    elif cmax == green:
+        hue = 60 * (((blue - red) / delta ) + 2)
+    elif cmax == blue:
+        hue = 60 * (((red - green) / delta ) + 4)
+
+    #calculate saturation
+    saturation = None
+    if cmax == 0:
+        saturation = 0
+    else:
+        saturation = delta / cmax * 100
+
+    value = cmax * 100
+
+    return (hue, saturation, value)
+
+
+
+def main():
+    color = "#F04C29"
+    rgb = hexToRGB(color)
+    hex = RGBtoHSV(rgb)
+
+    print(rgb)
+    print(hex)
+
+if __name__ == "__main__":
+    main()
--- process.py
+++ process.py
@@ -1,26 +1,94 @@
 import pandas
+import os
 
 def main():
 
-    file = open('matchups.csv', 'r')
+    #file = open('matchups.csv', 'r')
 
-    data = pandas.read_csv('matchups.csv')
+    #data = pandas.read_csv('matchups.csv')
 
     #print(data)
 
     #print(data[data['YEAR'] == 2024])
 
-    teamData = pandas.read_csv('mm-data/MTeams.csv')
+    #teamData = pandas.read_csv('mm-data/MTeams.csv')
 
-    print(teamIDtoName(teamData, 1439))
+    #print(teamIDtoName(teamData, 1439))
+    teams = generateTeams(64)
+    #printTeams(teams)
+    mups = teamsToMatchups(teams)
+    runBracket(mups)
 
 
+def runBracket(mups64):
+    mups32 = runRound(mups64, comp)
+    printMatchups(mups64)
+    print('NEXT ROUND')
+    mups16 = runRound(mups32, comp)
+    printMatchups(mups32)
+    print('NEXT ROUND')
+    printMatchups(mups16)
 
+def runRound(mups, compareTeams):
+    nxtRound = []
+    for mup in mups:
+        teams = mup['teams']
+        winner = compareTeams(teams[0], teams[1])
+        mup['winner'] = winner
+        nxtRound.append(teams[winner])
+    return teamsToMatchups(nxtRound)
+    
+    
+
+def comp(team1, team2):
+    return 1
+
+def teamsToMatchups(teams):
+    round_of = len(teams)
+    matchups = []
+    for i in range(0, round_of - 1, 2):
+        mup = {
+            #'matchup' : teams[i]['matchup'],
+            'matchup' : i // 2,
+            #'region' : i // 16,
+            'teams' : (teams[i], teams[i+1]),
+            'winner' : None
+        }
+        matchups.append(mup)
+    return matchups
 
 def teamIDtoName(teamData, id):
     #return teamData[teamData['TeamID'] == 1450]
     return teamData.loc[teamData['TeamID'] == id, 'TeamName'].iloc[0]
 
+def generateTeams(qty):
+    teams = []
+    for i in range(0, qty):
+        name = "Team #%d" % i 
+        team = { 'name' : name,
+                 'color' : randomHex(),
+                 'seed' : i % 16,
+                 'matchup' : i // 2 }
+        teams.append(team)
+    return teams
+
+def printTeams(teams):
+    for team in teams:
+        print(team)
+
+def printMatchups(mups):
+    for mup in mups:
+        print('Matchup #%d' % mup['matchup'])
+        #print('Region: %d' % mup['region'])
+        print(mup['teams'][0])
+        print(mup['teams'][1])
+        if mup['winner']: 
+            print('Winner: %s' % mup['teams'][mup['winner']]['name'])
+        #print('Winner: bonk')
+        print('----------')
+
+def randomHex():
+    return '#%s' % os.urandom(3).hex()
 
 if __name__ == "__main__":
     main()
