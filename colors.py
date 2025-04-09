edc1251ebd8d88749f5b887618c3dccc295d893f
justin-hubbard <jdawggey@gmail.com>
2025-04-08 23:45:59 -0700

    Conversion functions for RGB->XYZ->L*a*b*

--- colors.py
+++ colors.py
@@ -36,14 +36,63 @@ def RGBtoHSV(rgb):
 
     return (hue, saturation, value)
 
+def RGBtoXYZ(rgb):
+    nR = rgb[0] / 255
+    nG = rgb[1] / 255
+    nB = rgb[2] / 255
+
+    if ( nR > 0.04045 ): nR = pow(( nR + 0.055 ) / 1.055, 2.4)
+    else: nR = nR / 12.92
+
+    if ( nG > 0.04045 ): nG = pow(( nG + 0.055 ) / 1.055, 2.4)
+    else: nG = nG / 12.92
+
+    if ( nB > 0.04045 ): nB = pow(( nB + 0.055 ) / 1.055, 2.4)
+    else:nB = nB / 12.92
+
+    nR = nR * 100
+    nG = nG * 100
+    nB = nB * 100
+
+    X = nR * 0.4124 + nG * 0.3576 + nB * 0.1805
+    Y = nR * 0.2126 + nG * 0.7152 + nB * 0.0722
+    Z = nR * 0.0193 + nG * 0.1192 + nB * 0.9505
+
+    return (X, Y, Z)
+
+def XYZtoLab(xyz):
+    rX = xyz[0] / 95.047
+    rY = xyz[1] / 100.00
+    rZ = xyz[2] / 108.883
+
+    if ( rX > 0.008856 ): rX = pow(rX, 1/3 )
+    else: var_X = ( 7.787 * rX ) + ( 16 / 116 )
+
+    if ( rY > 0.008856 ): rY = pow(rY, 1/3 )
+    else: rY = ( 7.787 * rY ) + ( 16 / 116 )
+    
+    if ( rZ > 0.008856 ): rZ = pow(rZ, 1/3 )
+    else: rZ = ( 7.787 * rZ ) + ( 16 / 116 )
+
+    L = ( 116 * rY ) - 16
+    a = 500 * ( rX - rY )
+    b = 200 * ( rY - rZ )
+
+    return (L, a, b)
+
+
 
 def main():
     color = "#F04C29"
     rgb = hexToRGB(color)
-    hex = RGBtoHSV(rgb)
+    #hex = RGBtoHSV(rgb)
+
+    xyz = RGBtoXYZ(rgb)
+    lab = XYZtoLab(xyz)
 
     print(rgb)
-    print(hex)
+    print(xyz)
+    print(lab)
 
 if __name__ == "__main__":
     main()
